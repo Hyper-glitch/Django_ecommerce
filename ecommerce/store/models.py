8392b7714d8b8b60d01de4866d20c41f69967cca
Roman <graff049@gmail.com>
2021-12-28 19:45:08 +0300

    1. rename property for url img 2. add property to get info for total price and total count 3. add property for getting total price

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -22,7 +22,7 @@ class Product(models.Model):
         return self.name
 
     @property
-    def get_photo_url(self):
+    def get_img_url(self):
         try:
             url = self.image.url
         except:
@@ -46,6 +46,17 @@ class Order(models.Model):
     complete = models.BooleanField(default=False, null=True, blank=False)
     transaction_id = models.CharField(max_length=200, null=True)
 
+    @property
+    def get_total_data(self):
+        total_data = {
+            'total_items': '',
+            'total_price': '',
+        }
+        order_items = self.orderitem_set.all()
+        total_data['total_items'] = sum([item.quantity for item in order_items])
+        total_data['total_price'] = sum([item.get_total_price for item in order_items])
+        return total_data
+
     def __str__(self):
         return str(self.id)
 
@@ -55,6 +66,11 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total_price(self):
+        total_price = self.product.price * self.quantity
+        return total_price
 
 
 class ShippingAddress(models.Model):
